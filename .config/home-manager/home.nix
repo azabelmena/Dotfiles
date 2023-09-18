185bdc6b9dbbfd32094c39643dad25400af2f947
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2023-09-17 21:06:40 -0400

    Configuring first home-manager configuration file.

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -13,58 +13,46 @@
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-  home.stateVersion = "22.11"; # Please read the comment before changing.
+    home.stateVersion = "22.11"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
+    home.sessionVariables = {
+        EDITOR = "vim";
+    };
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+    programs.home-manager.enable = true;
 
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-  ];
+    home.packages = [
+        # Fonts
+        (pkgs.nerdfonts.override { fonts = ["IBMPlexMono"]; })
+    ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+    # Home Manager is pretty good at managing dotfiles. The primary way to manage
+    # plain files is through 'home.file'.
+    home.file = {
+    };
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
+    programs.git = {
+        enable = true;
+        userName = "Alec S. Zabel-Mena";
+        userEmail = "alec.zabel@upr.edu";
+        aliases = {
+            dot="/run/current-system/sw/bin/git --git-dir=$HOME --work-tree=$HOME";
+        };
+    };
 
-  # You can also manage environment variables but you will have to manually
-  # source
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/alec/etc/profile.d/hm-session-vars.sh
-  #
-  # if you don't want to manage your shell through Home Manager.
-  home.sessionVariables = {
-    # EDITOR = "emacs";
-  };
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+    gtk = {
+        enable = true;
+        theme = {
+            name = "rose-pine-gtk-theme";
+            package = pkgs.rose-pine-gtk-theme;
+        };
+        cursorTheme = {
+            name = "Bibata-Modern-Ice";
+            size = 24;
+        };
+        iconTheme = {
+            name = "rose-pine-icon-theme";
+            package = pkgs.rose-pine-icon-theme;
+        };
+    };
 }
