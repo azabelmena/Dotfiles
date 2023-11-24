f67f2a5b9f7102ae6042b874af66b4745fcd035c
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2023-11-24 13:23:33 -0400

    Removed prime settings. Cayley is a desktop.

--- .config/nixos/cayley/nvidia.nix
+++ .config/nixos/cayley/nvidia.nix
@@ -3,27 +3,23 @@
 
 # Cayley NIX!
 
-hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-};
+  hardware.opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+  };
 
-services.xserver.videoDrivers = ["nvidia"];
+  services.xserver.videoDrivers = ["nvidia"];
 
-hardware.nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = false;
-    powerManagement.finegrained = false;
+  hardware.nvidia = {
+      modesetting.enable = true;
+      powerManagement.enable = false;
+      powerManagement.finegrained = false;
 
-    open = true; # Open source driver (nvidia-open-dkms)
+      open = true; # Open source driver (nvidia-open-dkms)
 
-    nvidiaSettings = true;
+      nvidiaSettings = true;
 
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-
-    prime = {
-        nvidiaBusId = "PCI:1:0:0";
-        intelBusId = "PCI:0:2:0";};
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
     };
 }
