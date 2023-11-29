0ae3245fd02ccb02a183eb6f18a29d68d23b92d4
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2023-11-29 00:23:09 -0400

    Consolidated the NixOs configuration flake and the nix-darwin flake. Next is to consolidate home-manager.

--- .config/flake.nix
+++ .config/flake.nix
@@ -2,28 +2,28 @@
   description = "NixOS Configuration for Cayley and Cauchy.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    home-manager.url = "github:/nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    darwin.url = "github:LnL7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = {self, nixpkgs, ...}@inputs:
+  outputs = {self, nixpkgs, darwin, home-manager, ...}@inputs:
   let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      config = {
-        allowUnfree = true;
-      };
-    };
+    system-x86_64-linux = "x86_64-linux";
+    system-aarch64-darwin = "aarch64-darwin";
+
+    pkgs-nixos = import nixpkgs { inherit system-x86_64-linux; };
+    pkgs-darwin = import nixpkgs { inherit system-aarch64-darwin; };
+
   in{
     nixosConfigurations = {
 
       cayley = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs system; };
+        specialArgs = { inherit inputs system-x86_64-linux; };
 
         modules = [
           ./nixos/cayley/configuration.nix
@@ -31,7 +31,7 @@
       };
 
       cauchy = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs system; };
+        specialArgs = { inherit inputs system-x86_64-linux; };
 
         modules = [
           ./nixos/cauchy/configuration.nix
@@ -39,6 +39,19 @@
       };
 
     };
-  };
 
+    darwinConfigurations = {
+      noether = inputs.darwin.lib.darwinSystem{
+
+        modules = [
+          home-manager.darwinModules.home-manager{
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            #home-manager.users.alec = import ./home.nix;
+          }
+        ];
+
+      };
+    };
+  };
 }
