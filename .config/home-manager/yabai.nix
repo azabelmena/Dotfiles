f55cb9f89cadfb82cb3d6ff3ba63494a9327544b
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2023-12-18 17:07:49 -0400

    Added .nix files for yabai, skhd, and spacebar on nix-darwin.

--- .config/home-manager/yabai.nix
+++ .config/home-manager/yabai.nix
@@ -1,34 +1,31 @@
 { pkgs, ... }:
-
-  services = {
-    yabai = {
-      enable = true;
-      package = pkgs.yabai;
-      config = {
-        layout = "bsp";
-        auto_balance = "off";
-        split_ratio = "0.50";
-        window_border = "on";
-        window_border_width = "2";
-        window_placement = "second_child";
-        focus_follows_mouse = "autoraise";
-        mouse_follows_focus = "off";
-        top_padding = "10";
-        bottom_padding = "10";
-        left_padding = "10";
-        right_padding = "10";
-        window_gap = "10";
-      };
-      extraConfig = ''
-        yabai -m rule --add app='^Emacs$' manage=on
-        yabai -m rule --add title='Preferences' manage=off layer=above
-        yabai -m rule --add title='^(Opening)' manage=off layer=above
-        yabai -m rule --add title='Library' manage=off layer=above
-        yabai -m rule --add app='^System Preferences$' manage=off layer=above
-        yabai -m rule --add app='Activity Monitor' manage=off layer=above
-        yabai -m rule --add app='Finder' manage=off layer=above
-        yabai -m rule --add app='^System Information$' manage=off layer=above
-      '';
-    };
+{
+  enable = true;
+  enableScriptingAddition = true;
+  config = {
+    layout = "bsp";
+    auto_balance = "off";
+    split_ratio = "0.50";
+    window_border = "on";
+    window_border_width = "10";
+    window_placement = "second_child";
+    focus_follows_mouse = "off";
+    mouse_follows_focus = "off";
+    top_padding = "10";
+    bottom_padding = "10";
+    left_padding = "10";
+    right_padding = "10";
+    window_gap = "10";
   };
+  extraConfig=''
+    sudo yabai --load-sa
+    source "~/.config/colors.sh"
+    echo "yabai configuration loaded..."
+
+    yabai -m rule --add title='Preferences' manage=off layer=above
+    yabai -m rule --add app='^System Preferences$' manage=off layer=above
+    yabai -m rule --add app='Activity Monitor' manage=off layer=above
+    yabai -m rule --add app='Finder' manage=off layer=above
+    yabai -m rule --add app='^System Information$' manage=off layer=above
+  '';
 }
