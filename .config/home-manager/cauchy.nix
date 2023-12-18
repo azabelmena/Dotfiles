890ac69e0d6e0c83b6e1e217a155ad180ab0fde6
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2023-12-18 17:09:08 -0400

    Cleaned up styling and comments on cayley.nix and cauchy.nix

--- .config/home-manager/cauchy.nix
+++ .config/home-manager/cauchy.nix
@@ -1,69 +1,54 @@
 { config, pkgs, inputs, ... }:
 
 {
-    #imports = [
-        #inputs.nix-colors.homeManagerModules.default
-    #];
-    #colorScheme = inputs.nix-colors.colorSchemes.gruvbox-dark-medium;
 
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "alec";
   home.homeDirectory = "/home/alec";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-    home.stateVersion = "23.11"; # Please read the comment before changing.
+  home.stateVersion = "23.11";
 
-    home.sessionVariables = {
-        EDITOR = "vim";
-        #XDG_CONFIG_HOME = ".config";
-    };
+  home.sessionVariables = {
+      EDITOR = "vim";
+  };
 
-    programs.home-manager.enable = true;
+  programs.home-manager.enable = true;
 
-    home.packages = [
-        # Fonts
-        (pkgs.nerdfonts.override { fonts = ["IBMPlexMono"]; })
-    ];
+  home.packages = [
+      (pkgs.nerdfonts.override { fonts = ["IBMPlexMono"]; })
+  ];
 
-    programs = {
-        git = {
-            enable = true;
-            userName = "Alec S. Zabel-Mena";
-            userEmail = "alec.zabel@upr.edu";
-        };
-
-        bash = (import ./bash/bash.nix { inherit pkgs; });
-        zsh = (import ./zsh/zsh.nix { inherit pkgs; });
-        tmux = (import ./tmux.nix { inherit pkgs; });
-        kitty = (import ./kitty/kitty.nix { inherit pkgs; });
-        zathura = (import ./zathura.nix { inherit pkgs; });
-        rofi = (import ./rofi.nix { inherit pkgs; });
-        waybar = (import ./waybar/waybar.nix { inherit pkgs; });
-    };
-
-    home.file = {
-        ".local/share/zsh/zsh-autosuggestions".source =
-        "${pkgs.zsh-autosuggestions}/share/zhs-autosuggestions";
-        "~/.config/kitty/kitty.conf".source = "${pkgs.kitty}/kitty/kitty.conf";
-    };
-
-    gtk = {
+  programs = {
+      git = {
         enable = true;
-        theme = {
-        name = "adw-gtk3";
-        package = pkgs.adw-gtk3;
-        };
-        cursorTheme = {
-        name = "GoogleDot-Black";
-        package = pkgs.google-cursor;
-        size = 16;
-        };
-    };
+        userName = "Alec S. Zabel-Mena";
+        userEmail = "alec.zabel@upr.edu";
+      };
+
+      bash = (import ./bash/bash.nix { inherit pkgs; });
+      zsh = (import ./zsh/zsh.nix { inherit pkgs; });
+      tmux = (import ./tmux.nix { inherit pkgs; });
+      kitty = (import ./kitty/kitty.nix { inherit pkgs; });
+      zathura = (import ./zathura.nix { inherit pkgs; });
+      rofi = (import ./rofi.nix { inherit pkgs; });
+      waybar = (import ./waybar/waybar.nix { inherit pkgs; });
+  };
+
+  home.file = {
+      ".local/share/zsh/zsh-autosuggestions".source =
+      "${pkgs.zsh-autosuggestions}/share/zhs-autosuggestions";
+      "~/.config/kitty/kitty.conf".source = "${pkgs.kitty}/kitty/kitty.conf";
+  };
+
+  gtk = {
+      enable = true;
+      theme = {
+      name = "adw-gtk3";
+      package = pkgs.adw-gtk3;
+      };
+      cursorTheme = {
+      name = "GoogleDot-Black";
+      package = pkgs.google-cursor;
+      size = 16;
+      };
+  };
 }
--- .config/home-manager/cayley.nix
+++ .config/home-manager/cayley.nix
@@ -4,71 +4,59 @@
   imports = [
     inputs.nix-colors.homeManagerModules.default
   ];
-  #colorScheme = inputs.nix-colors.colorSchemes.gruvbox-dark-soft;
   colorScheme = inputs.nix-colors.colorSchemes.gruvbox-dark-soft;
 
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "alec";
   home.homeDirectory = "/home/alec";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-    home.stateVersion = "23.11"; # Please read the comment before changing.
+  home.stateVersion = "23.11";
 
-    home.sessionVariables = {
-        EDITOR = "vim";
-        #XDG_CONFIG_HOME = ".config";
-    };
+  home.sessionVariables = {
+      EDITOR = "vim";
+  };
 
-    programs.home-manager.enable = true;
+  programs.home-manager.enable = true;
 
-    home.packages = [
-        # Fonts
-        (pkgs.nerdfonts.override { fonts = ["IBMPlexMono"]; })
-    ];
+  home.packages = [
+      (pkgs.nerdfonts.override { fonts = ["IBMPlexMono"]; })
+  ];
 
-    programs = {
-        git = {
-            enable = true;
-            userName = "Alec S. Zabel-Mena";
-            userEmail = "alec.zabel@upr.edu";
-        };
+  programs = {
+      git = {
+          enable = true;
+          userName = "Alec S. Zabel-Mena";
+          userEmail = "alec.zabel@upr.edu";
+      };
 
-        bash = (import ./bash/bash.nix { inherit pkgs; });
-        kitty = (import ./kitty/kitty.nix { inherit pkgs config; });
-        qutebrowser = (import ./qutebrowser/qutebrowser.nix { inherit pkgs config; });
-        rofi = (import ./rofi.nix { inherit pkgs; });
-        tmux = (import ./tmux.nix { inherit pkgs; });
-        vim = (import ./vim.nix { inherit pkgs; });
-        waybar = (import ./waybar/waybar.nix { inherit pkgs; });
-        zathura = (import ./zathura.nix { inherit pkgs; });
-        zsh = (import ./zsh/zsh.nix { inherit pkgs; });
-    };
+      bash = (import ./bash/bash.nix { inherit pkgs; });
+      kitty = (import ./kitty/kitty.nix { inherit pkgs config; });
+      qutebrowser = (import ./qutebrowser/qutebrowser.nix { inherit pkgs config; });
+      rofi = (import ./rofi.nix { inherit pkgs; });
+      tmux = (import ./tmux.nix { inherit pkgs; });
+      vim = (import ./vim.nix { inherit pkgs; });
+      waybar = (import ./waybar/waybar.nix { inherit pkgs; });
+      zathura = (import ./zathura.nix { inherit pkgs; });
+      zsh = (import ./zsh/zsh.nix { inherit pkgs; });
+  };
 
-    home.file = {
-        ".local/share/zsh/zsh-autosuggestions".source =
-        "${pkgs.zsh-autosuggestions}/share/zhs-autosuggestions";
-        "~/.config/kitty/kitty.conf".source = "${pkgs.kitty}/kitty/kitty.conf";
-        "~/.config/qutebrowser/config.py".source = "${pkgs.qutebrowser}/qutebrowser/config.py";
-        "~/.config/qutebrowser/quickmarks".source = "${pkgs.qutebrowser}/qutebrowser/quickmarks";
-    };
+  home.file = {
+      ".local/share/zsh/zsh-autosuggestions".source =
+      "${pkgs.zsh-autosuggestions}/share/zhs-autosuggestions";
+      "~/.config/kitty/kitty.conf".source = "${pkgs.kitty}/kitty/kitty.conf";
+      "~/.config/qutebrowser/config.py".source = "${pkgs.qutebrowser}/qutebrowser/config.py";
+      "~/.config/qutebrowser/quickmarks".source = "${pkgs.qutebrowser}/qutebrowser/quickmarks";
+  };
 
-    gtk = {
-        enable = true;
-        theme = {
-        name = "adw-gtk3";
-        package = pkgs.adw-gtk3;
-        };
-        cursorTheme = {
-        name = "GoogleDot-Black";
-        package = pkgs.google-cursor;
-        size = 16;
-        };
-    };
+  gtk = {
+      enable = true;
+      theme = {
+      name = "adw-gtk3";
+      package = pkgs.adw-gtk3;
+      };
+      cursorTheme = {
+      name = "GoogleDot-Black";
+      package = pkgs.google-cursor;
+      size = 16;
+      };
+  };
 }
