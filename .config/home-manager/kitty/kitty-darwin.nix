95ab0d36a5ad119ed1be3e51f9eaf0d7ef16153e
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2023-12-18 17:08:42 -0400

    We nix-coloring now on nix-darwin!!

--- .config/home-manager/kitty/kitty-darwin.nix
+++ .config/home-manager/kitty/kitty-darwin.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
     enable = true;
 
@@ -17,29 +17,27 @@
     };
 
     settings = {
-        enable_audio_bell = "no";
-        remember_window_size = "yes";
-        confirm_os_window_close = "0";
-        macos_quit_when_last_window_closed = "yes";
-        hide_window_decorations = "titlebar-only";
+      enable_audio_bell = "no";
+      remember_window_size = "yes";
+      confirm_os_window_close = "0";
 
-        cursor = "#${config.colorScheme.colors.base06}";
-        cursor_text_color = "#${config.colorScheme.colors.base00}";
-        foreground = "#${config.colorScheme.colors.base06}";
-        background = "#${config.colorScheme.colors.base00}";
-        selection_background = "#${config.colorScheme.colors.base09}";
-        selection_foreground = "#${config.colorScheme.colors.base03}";
+      cursor = "#${config.colorScheme.colors.base06}";
+      cursor_text_color = "#${config.colorScheme.colors.base00}";
+      foreground = "#${config.colorScheme.colors.base06}";
+      background = "#${config.colorScheme.colors.base00}";
+      selection_background = "#${config.colorScheme.colors.base09}";
+      selection_foreground = "#${config.colorScheme.colors.base03}";
 
-        color0 = "#${config.colorScheme.colors.base00}";
-        color1 = "#${config.colorScheme.colors.base08}";
-        color2 = "#${config.colorScheme.colors.base0B}";
-        color3 = "#${config.colorScheme.colors.base0A}";
-        color4 = "#${config.colorScheme.colors.base0D}";
-        color5 = "#${config.colorScheme.colors.base0E}";
-        color6 = "#${config.colorScheme.colors.base0C}";
-        color7 = "#${config.colorScheme.colors.base06}";
-        color8 = "#${config.colorScheme.colors.base00}";
+      color0 = "#${config.colorScheme.colors.base00}";
+      color1 = "#${config.colorScheme.colors.base08}";
+      color2 = "#${config.colorScheme.colors.base0B}";
+      color3 = "#${config.colorScheme.colors.base0A}";
+      color4 = "#${config.colorScheme.colors.base0D}";
+      color5 = "#${config.colorScheme.colors.base0E}";
+      color6 = "#${config.colorScheme.colors.base0C}";
+      color7 = "#${config.colorScheme.colors.base06}";
+      color8 = "#${config.colorScheme.colors.base04}";
 
-        macos_colorspace = "srgb";
+      macos_colorspace = "srgb";
     };
 }
--- .config/home-manager/noether.nix
+++ .config/home-manager/noether.nix
@@ -1,38 +1,43 @@
-{ config, pkgs, inputs, lib, ... }:
+{ config, pkgs, inputs, ... }:
 
 {
+
+    imports = [
+      inputs.nix-colors.homeManagerModules.default
+    ];
+    colorScheme = inputs.nix-colors.colorSchemes.gruvbox-dark-soft;
+
     home.stateVersion = "23.11";
 
     home.sessionVariables = {
-        EDITOR = "vim";
-        XDG_CONFIG_HOME = ".config";
+    EDITOR = "vim";
+    XDG_CONFIG_HOME = ".config";
     };
 
     programs.home-manager.enable = true;
 
     home.packages = with pkgs; [
-        (nerdfonts.override { fonts = ["IBMPlexMono"]; })
+      (nerdfonts.override { fonts = ["IBMPlexMono"]; })
     ];
 
     programs = {
-        git = {
-            enable = true;
-            userName = "Alec S. Zabel-Mena";
-            userEmail = "alec.zabel@upr.edu";
-        };
+      git = {
+        enable = true;
+        userName = "Alec S. Zabel-Mena";
+        userEmail = "alec.zabel@upr.edu";
+      };
 
-        bash = (import ./bash/bash-darwin.nix { inherit pkgs; });
-        kitty = (import ./kitty/kitty-darwin.nix { inherit pkgs; });
-        #skhd = (import ./skhd.nix { inherit pkgs; });
-        tmux = (import ./tmux.nix { inherit pkgs; });
-        vim = (import ./vim.nix { inherit pkgs; });
-        #yabai = (import ./yabai.nix { inherit pkgs; });
-        zathura = (import ./zathura.nix { inherit pkgs; });
-        zsh = (import ./zsh/zsh-darwin.nix { inherit pkgs; });
+      bash = (import ./bash/bash-darwin.nix { inherit pkgs; });
+      kitty = (import ./kitty/kitty-darwin.nix { inherit pkgs config; });
+      tmux = (import ./tmux.nix { inherit pkgs; });
+      vim = (import ./vim.nix { inherit pkgs; });
+      zathura = (import ./zathura.nix { inherit pkgs; });
+      zsh = (import ./zsh/zsh-darwin.nix { inherit pkgs; });
     };
 
     home.file = {
-        ".local/share/zsh/zsh-autosuggestions".source =
-        "${pkgs.zsh-autosuggestions}/share/zhs-autosuggestions";
+      ".local/share/zsh/zsh-autosuggestions".source =
+      "${pkgs.zsh-autosuggestions}/share/zhs-autosuggestions";
+      "~/.config/kitty/kitty.conf".source = "${pkgs.kitty}/kitty/kitty.conf";
     };
 }
