b541104482510e52ad5973557f63e84dbbf87d5a
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2023-12-19 14:45:15 -0400

    Organizing vim.nix

--- .config/home-manager/vim.nix
+++ .config/home-manager/vim.nix
@@ -22,6 +22,47 @@
       set splitbelow
       set cursorline
       set cursorcolumn
+      set nobackup
+      set nowb
+      set noswapfile
+      set undodir=/home/alec/.vim/undodir
+      set undofile
+      set autoread
+      set so=7
+      set path+=**
+      set wildmenu
+      set ruler
+      set cmdheight=1
+      set hid
+      set backspace=eol,start,indent
+      set whichwrap+=<,>,h,l
+      set ignorecase
+      set smartcase
+      set hlsearch
+      set incsearch
+      set lazyredraw
+      set showmatch
+      set mat=2
+      set noerrorbells
+      set novisualbell
+      set t_vb=
+      set tm=500
+      set background=dark
+      set encoding=utf8
+      set fileformats=unix
+      set expandtab
+      set smarttab
+      set shiftwidth=4
+      set tabstop=4
+      set autoindent
+      set smartindent
+      set linebreak
+      set textwidth=80
+      set nowrap
+      set nolist
+      set formatoptions+=t
+      set colorcolumn=81
+
 
       let &t_SI = "\<esc>[6 q"
       let &t_SR = "\<esc>[4 q"
@@ -31,54 +72,15 @@
       filetype plugin on
       filetype indent on
 
-      set autoread
       au FocusGained,BufEnter * checktime
 
       let mapleader = ","
 
-      set so=7
-
-      set path+=**
-
-      set wildmenu
-
-      set ruler
-
-      set cmdheight=1
-
-      set hid
-
-      set backspace=eol,start,indent
-      set whichwrap+=<,>,h,l
-
-      set ignorecase
-
-      set smartcase
-
-      set hlsearch
-
-      set incsearch
-
       command C let @/=""
-
-      set lazyredraw
-
-      set showmatch
-      set mat=2
-
-      set noerrorbells
-      set novisualbell
-      set t_vb=
-      set tm=500
 
       syntax enable
 
-      set background=dark
       colorscheme gruvbox8_soft
-
-      set encoding=utf8
-
-      set fileformats=unix
 
       let g:netrw_banner = 0
       let g:netrw_browssplit = 0
@@ -142,24 +144,8 @@
       nnoremap <C-[> :bprevious<CR>
       nnoremap <C-p> :bdelete<CR>
 
-      set statusline+=%#warningmsg#
-      set statusline+=%{SyntasticStatuslineFlag()}
-      set statusline+=%*
-
       let g:airline_experimental = 1
       let g:airline_theme='base16_gruvbox_dark_hard'
-
-      let g:syntastic_always_populate_loc_list = 1
-      let g:syntastic_auto_loc_list = 0
-      let g:syntastic_check_on_open = 1
-      let g:syntastic_check_on_wq = 0
-
-      let g:syntastic_filetype_map = {
-          \ "plaintex": "tex",
-          \ "gentoo-metadata": "xml" }
-
-      let g:syntastic_mode_map = {
-          \ "passive_filetypes": ["tex", "md", "markdown", "s"] }
 
       let g:tex_flavor='latex'
       let g:vimtex_parser_bib_backend='bibtex'
@@ -169,13 +155,6 @@
       set conceallevel=1
       let g:tex_conceal='abdmg'
 
-      set nobackup
-      set nowb
-      set noswapfile
-
-      set undodir=/home/alec/.vim/undodir
-      set undofile
-
       autocmd BufRead, BufNewFile *.tex set filetype=tex
       autocmd BufRead, BufNewFile *.cu set filetype=cuda
 
@@ -183,23 +162,6 @@
 
       autocmd bufreadpre *.csv setlocal nowrap nolinebreak textwidth=0
       autocmd bufreadpre *.c setlocal nowrap nolinebreak textwidth=0
-
-      set expandtab
-
-      set smarttab
-
-      set shiftwidth=4
-      set tabstop=4
-
-      set autoindent
-      set smartindent
-
-      set linebreak
-      set textwidth=80        " Linebreak on 80 characters.
-      set nowrap              " Don't wrap the text around.
-      set nolist
-      set formatoptions+=t    " Keeps the linebreak formatting on editing lines.
-      set colorcolumn=81      " Set a color columnt to keep tabs on the margin.
 
       vnoremap <silent> * :<C-u>call VisualSelection(\'\', \'\')<CR>/<C-R>=@/<CR><CR>
       vnoremap <silent> # :<C-u>call VisualSelection(\'\', \'\')<CR>?<C-R>=@/<CR><CR>
