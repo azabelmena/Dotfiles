8c5fb288222958511512c983b16cd4c6c9a7d785
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2023-12-26 18:23:00 -0400

    Seperated home.file variable into its own nix module; I don't want my home.nix (cayley.nix, etc.) getting too cluttered.

--- .config/home-manager/cayley.nix
+++ .config/home-manager/cayley.nix
@@ -42,30 +42,7 @@
       zsh = (import ./zsh/zsh.nix { inherit pkgs; });
   };
 
-    home.file = {
-      ".local/share/zsh/zsh-autosuggestions".source =
-      "${pkgs.zsh-autosuggestions}/share/zhs-autosuggestions";
-
-      "nvim" = {
-        recursive = true;
-        source = config.lib.file.mkOutOfStoreSymlink
-        "/home/alec/.config/home-manager/nvim/lua/";
-        target = ".config/nvim/lua/";
-      };
-
-      "ultisnips" = {
-        recursive = true;
-        source = config.lib.file.mkOutOfStoreSymlink
-        "/home/alec/.config/home-manager/nvim/UltiSnips";
-        target = ".config/nvim/UltiSnips/";
-      };
-
-      "waybar" = {
-        source = config.lib.file.mkOutOfStoreSymlink
-        "/home/alec/.config/home-manager/waybar/style.css";
-        target = ".config/waybar/style.css";
-      };
-  };
+    home.file = ( import ./file.nix { inherit pkgs config lib; });
 
   gtk = {
       enable = true;
