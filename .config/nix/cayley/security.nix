624ba484720b2d0418cbae9da6d04a8aa71be4f0
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2023-12-28 12:32:35 -0400

    Testing security and polkit (should probably be done on testing).

--- .config/nix/cayley/security.nix
+++ .config/nix/cayley/security.nix
@@ -2,8 +2,20 @@
 {
   rtkit.enable = true;
 
+  audit = {
+    enable = true;
+
+    backlogLimit = 512;
+    rateLimit = 1000;
+  };
+  auditd.enable = true;
+
   polkit = {
     enable = true;
+    debug = true;
+    adminIdentities = [
+      "unix-group:wheel"
+    ];
   };
 
   pam.services.swaylock = {
