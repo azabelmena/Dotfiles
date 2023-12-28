1d109595c48142e7a6dfef50869d2e12b000ff5e
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2023-12-27 23:17:04 -0400

    Added qutebrowser config files to cayley.

--- .config/home-manager/noether.nix
+++ .config/home-manager/noether.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, inputs, ... }: #NOETHER NIX!
+{ config, pkgs, inputs, lib, ... }: #NOETHER NIX!
 
 {
     imports = [
@@ -28,34 +28,12 @@
 
       bash = (import ./bash/bash-darwin.nix { inherit pkgs; });
       kitty = (import ./kitty/kitty-darwin.nix { inherit pkgs config; });
+      neovim = ( import ./nvim/nvim.nix { inherit pkgs config; } );
+      qutebrowser = (import ./qutebrowser/qutebrowser.nix { inherit pkgs config; });
       starship = ( import ./starship.nix { inherit pkgs config; });
       tmux = (import ./tmux.nix { inherit pkgs; });
-      neovim = ( import ./nvim/nvim.nix { inherit pkgs config; } );
       zsh = (import ./zsh/zsh-darwin.nix { inherit pkgs; });
     };
 
-    home.file = {
-      ".local/share/zsh/zsh-autosuggestions".source =
-      "${pkgs.zsh-autosuggestions}/share/zhs-autosuggestions";
-
-      "nvim" = {
-        recursive = true;
-        source = config.lib.file.mkOutOfStoreSymlink
-        "/Users/alec/.config/home-manager/nvim/lua/";
-        target = ".config/nvim/lua/";
-      };
-
-      "ultisnips" = {
-        recursive = true;
-        source = config.lib.file.mkOutOfStoreSymlink
-        "/Users/alec/.config/home-manager/nvim/UltiSnips";
-        target = ".config/nvim/UltiSnips/";
-      };
-
-      "waybar" = {
-        source = config.lib.file.mkOutOfStoreSymlink
-        "/Users/alec/.config/home-manager/waybar/style.css";
-        target = ".config/waybar/style.css";
-      };
-  };
+  home.file = ( import ./files/files-darwin.nix { inherit pkgs config lib; });
 }
