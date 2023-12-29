f4c843b2d40382a3ac2560b78aa36ca930464d7f
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2023-12-29 00:18:16 -0400

    Reworked file paths.

--- .config/home-manager/files/files.nix
+++ .config/home-manager/files/files.nix
@@ -6,27 +6,27 @@
   "nvim" = {
     recursive = true;
     source = config.lib.file.mkOutOfStoreSymlink
-    "/home/alec/.config/home-manager/nvim/lua/";
+    "${config.home.homeDirectory}/.config/home-manager/nvim/lua/";
     target = ".config/nvim/lua/";
   };
 
   "ultisnips" = {
     recursive = true;
     source = config.lib.file.mkOutOfStoreSymlink
-    "/home/alec/.config/home-manager/nvim/UltiSnips";
+    "${config.home.homeDirectory}/.config/home-manager/nvim/UltiSnips";
     target = ".config/nvim/UltiSnips/";
   };
 
   "waybar" = {
     source = config.lib.file.mkOutOfStoreSymlink
-    "/home/alec/.config/home-manager/waybar/style.css";
+    "${config.home.homeDirectory}/.config/home-manager/waybar/style.css";
     target = ".config/waybar/style.css";
   };
 
   "fastfetch" = {
     recursive = true;
     source = config.lib.file.mkOutOfStoreSymlink
-    "/home/alec/.config/home-manager/fastfetch/config.jsonc";
+    "${config.home.homeDirectory}/.config/home-manager/fastfetch/config.jsonc";
     target = ".config/fastfetch/config.jsonc";
   };
 }
