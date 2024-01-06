f53087b5b7e2ad7f4983b2f5c9289c76d55cbb33
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2024-01-06 16:21:41 -0400

    Fixed typo.

--- .config/nix/lovelace/users.nix
+++ .config/nix/lovelace/users.nix
@@ -17,12 +17,12 @@
       tmux
       zsh
     ];
-  };
 
-  openssh = {
-    autrorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPF3kIluoKiCX45C1uIkp6axm5TSXvnj3ziu+hXjBF0U alec@cayley"
-    ];
+    openssh = {
+      autrorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPF3kIluoKiCX45C1uIkp6axm5TSXvnj3ziu+hXjBF0U alec@cayley"
+      ];
+    };
   };
 
 }
