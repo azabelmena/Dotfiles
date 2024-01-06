b9b33046707390b7c4be36b1012dd93c036f5186
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2024-01-06 16:19:49 -0400

    Updated ssh-key for lovelace.

--- .config/nix/lovelace/users.nix
+++ .config/nix/lovelace/users.nix
@@ -21,7 +21,7 @@
 
   openssh = {
     autrorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPF3kIluoKiCX45C1uIkp6axm5TSXvnj3ziu+hXjBF0U alec@cayley";
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPF3kIluoKiCX45C1uIkp6axm5TSXvnj3ziu+hXjBF0U alec@cayley"
     ];
   };
 
