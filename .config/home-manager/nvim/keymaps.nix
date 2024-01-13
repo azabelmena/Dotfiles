a63627dc3919688260c8cfe93e1e8e07578bafee
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2024-01-13 00:41:08 -0400

    Added keymaps.

--- .config/home-manager/nvim/keymaps.nix
+++ .config/home-manager/nvim/keymaps.nix
@@ -41,4 +41,105 @@
     action = "<C-w>l";
   }
 
+  {
+    key = "$";
+    action = "g$";
+  }
+  {
+    key = "j";
+    action = "gj";
+  }
+  {
+    key = "k";
+    action = "gk";
+  }
+  {
+    key = "0";
+    action = "g0";
+  }
+
+  {
+    key = "<C-]>";
+    action = ":bnext<CR>";
+  }
+  {
+    key = "<C-[>";
+    action = ":bprev<CR>";
+  }
+  {
+    key = "<C-p>";
+    action = ":bdelete<CR>";
+  }
+
+  {
+      key = "<leader>ss";
+      action = ":setlocal spell!<CR>";
+  }
+  {
+      key = "<leader>sn";
+      action = "]s";
+  }
+  {
+      key = "<leader>sp";
+      action = "[s";
+  }
+  {
+      key = "<leader>sa";
+      action = "zg";
+  }
+  {
+      key = "<leader>s?";
+      action = "z=";
+  }
+
+  {
+      key = "n";
+      action = "nzz";
+  }
+  {
+    key = "N";
+    action = "Nzz";
+  }
+
+  {
+    key = "<ESC>";
+    action = "<ESC>";
+    mode = [ "n" "i" "v" ];
+  }
+  {
+    key = "<Bksp>";
+    action = "<Bksp>";
+    mode = [ "n" "i" "v" ];
+  }
+  {
+    key = "<Up>";
+    action = "<Nop>";
+    mode = [ "n" "i" "v" ];
+  }
+  {
+    key = "<Left>";
+    action = "<Nop>";
+    mode = [ "n" "i" "v" ];
+  }
+  {
+    key = "<Right>";
+    action = "<Nop>";
+    mode = [ "n" "i" "v" ];
+  }
+  {
+    key = "<Down>";
+    action = "<Nop>";
+    mode = [ "n" "i" "v" ];
+  }
+  {
+    key = "<PageUp>";
+    action = "<Nop>";
+    mode = [ "n" "i" "v" ];
+  }
+  {
+    key = "<PageDown>";
+    action = "<Nop>";
+    mode = [ "n" "i" "v" ];
+  }
+
 ]
