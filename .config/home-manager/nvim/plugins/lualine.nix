1114139921329e5d1cf9c227758a5f0596f3056c
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2024-01-13 10:01:13 -0400

    Added lualine configuration.

--- .config/home-manager/nvim/plugins/lualine.nix
+++ .config/home-manager/nvim/plugins/lualine.nix
@@ -2,4 +2,20 @@
 
   enable = true;
 
+  alwaysDivideMiddle = true;
+  globalstatus = true;
+  iconsEnabled = true;
+
+  theme = "auto";
+
+  componentSeparators = {
+    left = "";
+    right = "";
+  };
+
+  sectionSeparators = {
+    left = "";
+    right = "";
+  };
+
 }
