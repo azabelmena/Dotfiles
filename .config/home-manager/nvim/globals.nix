38981da795c4735280635210d055ba1cd1b48b4e
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2024-01-13 00:40:46 -0400

    Added global options.

--- .config/home-manager/nvim/globals.nix
+++ .config/home-manager/nvim/globals.nix
@@ -1,6 +1,9 @@
 {
-
   mapleader = ",";
-  localleader = "\\";
-  
+  maplocalleader = "\\";
+
+  netrw_banner = 0;
+  netrw_browse_split = 0;
+  netrw_browse_liststyle = 0;
+  netrw_bufsettings = "noma nomod nonu nowrap ro nobl";
 }
