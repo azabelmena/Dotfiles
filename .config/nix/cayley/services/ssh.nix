49e7377a398eedd83c39f353fb505610be2754cf
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2024-02-06 23:43:02 -0400

    Update ssh.nix config for sshd.

--- .config/nix/cayley/services/ssh.nix
+++ .config/nix/cayley/services/ssh.nix
@@ -3,16 +3,28 @@
 
   settings = {
     UseDns = true;
-    PasswordAuthentication = true;
+
+    PasswordAuthentication = false;
     PermitRootLogin = "no";
+
     GatewayPorts = "yes";
+
+    AllowUsers = [ "alec" ];
+
+    Ciphers = [
+      "aes256-ctr"
+      "aes256-gcm@openssh.com"
+      "chacha20-poly1305@openssh.com"
+    ];
+
+    AllowTcpForwarding = true;
+    LogLevel = "VERBOSE";
   };
+
 
   allowSFTP = false;
 
   ports = [
-    22
-    2222
     4096
   ];
 }
