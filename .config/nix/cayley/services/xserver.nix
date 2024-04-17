5039f3ad16d151bb3a2bd1fe05da273d7345d2bb
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2024-04-16 23:59:34 -0400

    Updated services files to reflect the current nix options regarding them (after the flake update).

--- .config/nix/cayley/services/xserver.nix
+++ .config/nix/cayley/services/xserver.nix
@@ -1,17 +1,5 @@
-{ pkgs, ... }:
 {
-
-#`services.xserver.layout
-#`services.xserver.xkb.layout'.
-
   enable = true;
   xkb.layout = "us";
   videoDrivers = ["nvidia"];
-  displayManager = {
-    sddm = {
-      enable = true;
-      theme = "${import ./sddm.nix { inherit pkgs; }}";
-      autoNumlock = false;
-    };
-  };
 }
